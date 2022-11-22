32d4d44ddd8250e3f03711d6f9ea58bff38b0b6c
Torekhanova Fariza <118828305+torekhanovafariza@users.noreply.github.com>
2022-11-22 20:10:05 +0500

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,14 @@
+while True:
+    i = input("login : ")
+    s = input("parol:")
+    
+    if i == "1235" and s == "Turekanova":
+        print("kirildi")
+        break
+    elif i == "1235" and s  == "Turekanova":
+        print("ytd xato")
+    elif i == "1235" and s  != "Turekanova":
+        print("parol xato")    
+    else:
+        ("login x parol xato")
+        #fariza#2004
